d90be0f0c17c63d6d0c878676675b5d8dfec56b1
Weng Fei Fung <weffung@ucdavis.edu>
2023-02-04 22:20:13 -0800

    Setup event listeners and data structures

--- assets/piano.js
+++ assets/piano.js
@@ -1,3 +1,18 @@
+// For VS Editor Outline
+delete {
+    ".Setup":{
+        "Setup data structures":{},
+        "Setup event listeners":{}
+    },
+    "Intervals":{
+        "Calculate interval difference":{},
+    },
+}
+
+// Setup data structures. Initiate with number of keys
+window.keys = new Array(38)
+
+// Setup event listeners
 document.querySelectorAll(".position").forEach(el=>{
     el.addEventListener("click", event=>{
         el.classList.add("highlighted")
