6db1d2731d649f69e54ae9ad0b508627c99acd11
Weng Fei Fung <weffung@ucdavis.edu>
2023-02-06 01:34:17 -0800

    Adjusting algorithm on playing tune while holding note to reduce clipping noise as much as possible

--- assets/notesplayer.js
+++ assets/notesplayer.js
@@ -18,17 +18,20 @@
  */
 class NotesPlayer {
   constructor() {
-     this.NOTES = {"PAUSE": 0.1, "C": 261.63,"C#": 277.18,"Db": 277.18,"D": 293.66,"D#": 311.13,"Eb": 311.13,"E": 329.63,"F": 349.23,"F#": 369.99,"Gb": 369.99,"G": 392,"G#": 415.30,"Ab":415.30,"A": 440,"A#": 466.16,"Bb": 466.16,"B": 493.88 };
+    this.NOTES = { "PAUSE": 0.1, "C": 261.63, "C#": 277.18, "Db": 277.18, "D": 293.66, "D#": 311.13, "Eb": 311.13, "E": 329.63, "F": 349.23, "F#": 369.99, "Gb": 369.99, "G": 392, "G#": 415.30, "Ab": 415.30, "A": 440, "A#": 466.16, "Bb": 466.16, "B": 493.88 };
 
-     let audioContextClass = window.AudioContext || window.webkitAudioContext;
-     this.context = new audioContextClass();
+    let audioContextClass = window.AudioContext || window.webkitAudioContext;
+    this.context = new audioContextClass();
   }
 } // NotesPlayer
 
 /**  
 * @function
-* Obsoleted: Kept here just in case. Is obsoleted by another function that reduces the click sound based on this article:
+* TODO: Playing around with toggling between the two play algorithms below by having the other one disabled being named to play1 rather than play. 
+*       Figuring out how to reduce clip noise:
 *            http://alemangui.github.io/blog//2015/12/26/ramp-to-value.html
+*            https://stackoverflow.com/questions/29378875/how-can-i-avoid-this-clicking-sound-when-i-stop-playing-a-sound
+*             
 * Play note given musical note, octave (), duration (in seconds with decimals, eg. 2.33), 
 *
 * @param {string} note - musical note (eg. Ab, A, A#)
@@ -37,20 +40,20 @@ class NotesPlayer {
 * @param {float} durationSecondsPerBeat - OPTIONAL. eg. 1, 2, 2.33. If duration is not provided, then we are assuming 4 notes per measure and the duration per beat will be calculated by the BPM; Otherwise, you could pass a duration for that note to override that calculation (and you could pass null to bpm).
 *
 */
-NotesPlayer.prototype.playObsoleted = function(note, octave, bpm=60, durationSecondsPerBeat=1/(bpm/60)) {
+NotesPlayer.prototype.play = function (note, octave, bpm = 60, durationSecondsPerBeat = 1 / (bpm / 60)) {
   let context = this.context,
-      freq = this.NOTES[note],
-      octaveFactor = Math.pow(2, octave);
+    freq = this.NOTES[note],
+    octaveFactor = Math.pow(2, octave);
 
-  console.log(`Playing note ${note} at ${octave===0?"same octave":(octave>0?"+"+octave+" octave":octave+" octave")} for ${durationSecondsPerBeat} seconds at ${bpm}bpm`);
+  console.log(`Playing note ${note} at ${octave === 0 ? "same octave" : (octave > 0 ? "+" + octave + " octave" : octave + " octave")} for ${durationSecondsPerBeat} seconds at ${bpm}bpm`);
 
   var o = context.createOscillator();
   o.frequency.setTargetAtTime(freq * octaveFactor, context.currentTime, 0);
   g = context.createGain();
-  g.gain.setTargetAtTime(durationSecondsPerBeat, context.currentTime, 0);
+  g.gain.setTargetAtTime(.310, context.currentTime, .015);
   o.start(0);
   o.connect(context.destination);
-  o.stop(context.currentTime + durationSecondsPerBeat);
+  o.stop(context.currentTime + .310);
 }
 
 
@@ -64,25 +67,25 @@ NotesPlayer.prototype.playObsoleted = function(note, octave, bpm=60, durationSec
 * @param {float} durationSecondsPerBeat - OPTIONAL. eg. 1, 2, 2.33. If duration is not provided, then we are assuming 4 notes per measure and the duration per beat will be calculated by the BPM; Otherwise, you could pass a duration for that note to override that calculation (and you could pass null to bpm).
 *
 */
-NotesPlayer.prototype.play = function(note, octave=0, bpm=60, durationSecondsPerBeat=1/(bpm/60)) {
-let context = this.context,
+NotesPlayer.prototype.play2 = function (note, octave = 0, bpm = 60, durationSecondsPerBeat = 1 / (bpm / 60)) {
+  let context = this.context,
     freq = this.NOTES[note],
     octaveFactor = Math.pow(2, octave);
 
-console.log(`Playing note ${note} at ${octave===0?"same octave":(octave>0?"+"+octave+" octave":octave+" octave")} for ${durationSecondsPerBeat} seconds at ${bpm}bpm`);
+  console.log(`Playing note ${note} at ${octave === 0 ? "same octave" : (octave > 0 ? "+" + octave + " octave" : octave + " octave")} for ${durationSecondsPerBeat} seconds at ${bpm}bpm`);
 
-var o = context.createOscillator();
-o.frequency.setTargetAtTime(freq * octaveFactor, context.currentTime, 0);
-g = context.createGain();
-o.connect(g);
-g.connect(context.destination);
-o.start();
+  var o = context.createOscillator();
+  o.frequency.setTargetAtTime(freq * octaveFactor, context.currentTime, 0);
+  g = context.createGain();
+  o.connect(g);
+  g.connect(context.destination);
+  o.start();
 
-// Formula:
-// g.gain.setTargetAtTime(0, context.currentTime + SECONDS_OF_PLAYING, TRANSITIONING);
-// TRANSITIONING is the timeConstraint parameter:
-// https://developer.mozilla.org/en-US/docs/Web/API/AudioParam/setTargetAtTime
-g.gain.setTargetAtTime(0, context.currentTime + .3, 0);
+  // Formula:
+  // g.gain.setTargetAtTime(0, context.currentTime + SECONDS_OF_PLAYING, TRANSITIONING);
+  // TRANSITIONING is the timeConstraint parameter:
+  // https://developer.mozilla.org/en-US/docs/Web/API/AudioParam/setTargetAtTime
+  g.gain.setTargetAtTime(0, context.currentTime + .310, .001);
 }
 
 /**  
@@ -94,13 +97,13 @@ g.gain.setTargetAtTime(0, context.currentTime + .3, 0);
 * 
 */
 
-NotesPlayer.prototype.pause = function(ms) {
+NotesPlayer.prototype.pause = function (ms) {
   console.log(`Pausing for ${ms} milliseconds.`);
 
   const startTime = new Date().getTime();
-  for(var i = 0; i < 1e14; i++) { // originally 1e7 but 1e14 is more precise when 30bpm and you dont want overlapping beats
-      let currentTime = new Date().getTime();
-      if( (currentTime - startTime) > ms ) break;
+  for (var i = 0; i < 1e14; i++) { // originally 1e7 but 1e14 is more precise when 30bpm and you dont want overlapping beats
+    let currentTime = new Date().getTime();
+    if ((currentTime - startTime) > ms) break;
   } // for
 }
 
